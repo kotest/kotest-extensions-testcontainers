1adf54b470ffb90f3886710899f27a5286bf9f69
sksamuel <sam@sksamuel.com>
2021-04-24 10:41:44 -0500

    Migrated project from top level repo

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,22 @@
+object Libs {
+
+   const val kotlinVersion = "1.4.32"
+   const val org = "io.kotest.extensions"
+
+   object Kotest {
+      private const val version = "4.4.3"
+      const val junit5 = "io.kotest:kotest-runner-junit5-jvm:$version"
+      const val Api = "io.kotest:kotest-framework-api:$version"
+   }
+
+   object TestContainers {
+      private const val version = "1.15.2"
+      const val testcontainers = "org.testcontainers:testcontainers:$version"
+   }
+
+   object Coroutines {
+      private const val version = "1.4.3"
+      const val coreCommon = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+      const val coreJvm = "org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:$version"
+   }
+}
