1f3f6bcc0d9de129b505addeff7bf12fa05018a6
sksamuel <sam@sksamuel.com>
2021-08-01 11:56:30 -0500

    Bumped version of test containers

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -11,7 +11,7 @@ object Libs {
    }
 
    object TestContainers {
-      private const val version = "1.15.2"
+      private const val version = "1.15.3"
       const val testcontainers = "org.testcontainers:testcontainers:$version"
    }
 
