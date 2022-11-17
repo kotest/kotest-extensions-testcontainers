e9a4ca4879998be5c62604220593c9d468de16df
Emil Kantis <emil.kantis@protonmail.com>
2022-11-17 19:57:29 +0100

    Sonatype s01

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ allprojects {
    repositories {
       mavenCentral()
       maven {
-         url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+         url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
       }
    }
 
