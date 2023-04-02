fce1b5926ef9b0a5c6771fd5972f91621c7aa259
sksamuel <sam@sksamuel.com>
2023-04-02 17:15:00 -0500

    Added AbstractContainerExtension

--- kotest-extensions-testcontainers-localstack/src/main/kotlin/io/kotest/extensions/testcontainers/localstack/LocalStackContainerExtension.kt
+++ kotest-extensions-testcontainers-localstack/src/main/kotlin/io/kotest/extensions/testcontainers/localstack/LocalStackContainerExtension.kt
@@ -6,20 +6,16 @@ import io.kotest.core.listeners.AfterSpecListener
 import io.kotest.core.listeners.AfterTestListener
 import io.kotest.core.listeners.BeforeSpecListener
 import io.kotest.core.listeners.BeforeTestListener
-import io.kotest.core.spec.Spec
-import io.kotest.core.test.TestCase
-import io.kotest.core.test.TestResult
-import io.kotest.core.test.TestType
+import io.kotest.extensions.testcontainers.AbstractContainerExtension
 import io.kotest.extensions.testcontainers.TestContainerLifecycleMode
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import org.testcontainers.containers.localstack.LocalStackContainer
 import org.testcontainers.utility.DockerImageName
 
 class LocalStackContainerExtension(
    image: DockerImageName,
    private val mode: TestContainerLifecycleMode,
-) : MountableExtension<LocalStackContainer, LocalStackContainer>,
+) : AbstractContainerExtension(mode),
+   MountableExtension<LocalStackContainer, LocalStackContainer>,
    BeforeSpecListener,
    AfterSpecListener,
    BeforeTestListener,
@@ -27,44 +23,15 @@ class LocalStackContainerExtension(
    AfterProjectListener {
 
    constructor() : this(DockerImageName.parse("localstack/localstack:0.11.3"), TestContainerLifecycleMode.Project)
-
    constructor(mode: TestContainerLifecycleMode) : this(DockerImageName.parse("localstack/localstack:0.11.3"), mode)
 
    private val localstack = LocalStackContainer(image)
 
+   override fun getContainer() = localstack
+
    override fun mount(configure: LocalStackContainer.() -> Unit): LocalStackContainer {
       localstack.configure()
-      if (mode == TestContainerLifecycleMode.Project) localstack.start()
+      super.onMount()
       return localstack
    }
-
-   override suspend fun afterProject() {
-      if (mode == TestContainerLifecycleMode.Project && localstack.isRunning)
-         withContext(Dispatchers.IO) { localstack.stop() }
-   }
-
-   private fun isTestCase(testCase: TestCase) =
-      mode == TestContainerLifecycleMode.Test ||
-         (mode == TestContainerLifecycleMode.Leaf && testCase.type == TestType.Test)
-
-   override suspend fun beforeAny(testCase: TestCase) {
-      if (isTestCase(testCase))
-         withContext(Dispatchers.IO) { localstack.start() }
-   }
-
-   override suspend fun afterAny(testCase: TestCase, result: TestResult) {
-      if (isTestCase(testCase) && localstack.isRunning)
-         withContext(Dispatchers.IO) { localstack.stop() }
-   }
-
-   override suspend fun beforeSpec(spec: Spec) {
-      if (mode == TestContainerLifecycleMode.Spec)
-         withContext(Dispatchers.IO) { localstack.start() }
-   }
-
-   override suspend fun afterSpec(spec: Spec) {
-      if (mode == TestContainerLifecycleMode.Spec && localstack.isRunning)
-         withContext(Dispatchers.IO) { localstack.stop() }
-   }
-
 }
